c6abca189eb40377c93f42193e4fcc2678ea0507
multimediq <158053345+multimediq@users.noreply.github.com>
2026-04-29 15:04:54 +0100

    Add files via upload
    Configuração de parâmetros de unidades (Celsius) e idioma (PT)

--- api.js
+++ api.js
@@ -1,17 +1,25 @@
 const API_KEY = '9c24f65137121dfa3f0f5ea0790c188c';
 
+// Interação 1: Clima Atual
 async function getWeatherData(cidade) {
     try {
-        // O link tem de incluir a tua API_KEY no fim!
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${API_KEY}&units=metric&lang=pt`);
-        
         if (!response.ok) throw new Error('Cidade não encontrada');
-        
-        const data = await response.json();
-        console.log("Dados recebidos:", data); // Para veres na consola se funcionou
-        return data;
+        return await response.json();
     } catch (error) {
-        console.error("Erro na API:", error);
+        console.error(error);
+        return null;
+    }
+}
+
+// Interação 2: Previsão 5 Dias
+async function getForecastData(cidade) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=${API_KEY}&units=metric&lang=pt`);
+        const data = await response.json();
+        // Filtra para pegar apenas uma leitura por dia (a cada 24h)
+        return data.list.filter((_, index) => index % 8 === 0);
+    } catch (error) {
         return null;
     }
 }
